da3430997e78c3ff8e759ddfc9f14618b5c761a9
github-action-benchmark <github@users.noreply.github.com>
2026-01-27 08:56:46 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 09ad3066ec9435b1de53a9c43b28837b3b087e0e

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767251501641,
+  "lastUpdate": 1769504206971,
   "repoUrl": "https://github.com/Amama-Fatima/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Amama-Fatima/n8n-mcp/commit/9ffa1b1f0f8cf07ff796d28cc0e673dce9cfc587"
         },
         "date": 1767251501161,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "115226955+Amama-Fatima@users.noreply.github.com",
+            "name": "Amama Fatima",
+            "username": "Amama-Fatima"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09ad3066ec9435b1de53a9c43b28837b3b087e0e",
+          "message": "Merge branch 'czlonkowski:main' into main",
+          "timestamp": "2026-01-27T13:54:25+05:00",
+          "tree_id": "019389fbf33649e5c0b9022022a3bda047ab9625",
+          "url": "https://github.com/Amama-Fatima/n8n-mcp/commit/09ad3066ec9435b1de53a9c43b28837b3b087e0e"
+        },
+        "date": 1769504206295,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
